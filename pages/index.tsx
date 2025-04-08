6d1064a4f205f003c265370c05f7bc94fbea0d14
jhair <jinfanzonquispe30@gmail.com>
2025-04-07 19:07:41 -0500

    add projectr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,113 +1,69 @@
+import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// // const geistSans = Geist({
+// //   variable: "--font-geist-sans",
+// //   subsets: ["latin"],
+// // });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="">
+      <Head>
+        <title>AppSorteos – Generador de Equipos Aleatorios</title>
+      </Head>
+      <nav className=" max-w-[1292px] mx-auto flex py-[16px] w-full">
+        <picture className="block h-[38px]"> <Image className="w-full h-full object-contain" src={"https://app-sorteos.com/img/logo.svg"} width={714.219} height={167.126} alt=""></Image></picture>
+      </nav>
+      <div className="bg-[#f3f3fa] py-[8rem]">
+        <div className="max-w-[110rem] mx-auto">
+          <h1 className="text-[3rem] font-extrabold">Generador de Equipos y Grupos Aleatorios</h1>
+          <p>Crea  <b>equipos al azar</b> a partir de una lista de nombres de forma automática y gratuita.</p>
+          <div className=" w-[110rem] mx-auto bg-white px-[2rem] py-[2rem] my-[3rem] flex gap-[4rem]">
+            <div className="w-1/2 ">
+              <p className="text-[1.6rem] mb-[1.5rem] font-bold" >1. Ingresa los participantes</p>
+              <textarea name="" id="lista" className="border-[0.1rem] border-[#d1d1d1] rounded-md w-full h-[25.3rem] px-[1.4rem] py-[1rem] max-h-[25.3rem] overflow-y-auto focus:border-[#d31c92] focus:border-2 focus:outline-0"></textarea></div>
+            <div className="w-1/2">
+              <p className="text-[1.6rem] font-bold" >2. Cómo dividir:</p>
+              <div className="flex gap-5 my-[2rem]">
+                <label className="flex gap-2">
+                  <input
+                  className="accent-[#d31c92] w-[2rem] h-[2rem] "
+                    type="radio"
+                    name="opciones"
+                    value="option1"
+                    checked={true}
+                    onChange={()=>{}}
+                  />
+                  Cantidad de equipos
+
+                </label>
+                <label className="flex gap-2">
+                  <input
+                   className="accent-[#d31c92] w-[2rem] h-[2rem]"
+                    type="radio"
+                    name="opciones"
+                    value="option1"
+                    checked={true}
+                    onChange={()=>{}}
+                  />
+                  Participantes por equipo
+
+                </label>
+                
+              </div>
+              <div>
+                    <input className="w-full border border-[#d1d1d1] text-[#797979] py-5 px-5" type="text" value={"4 equipos  ✓"} />
+                </div>
+              <p className="text-[1.6rem] font-bold  my-[2rem]" >3. Título</p>
+            </div>
+
+
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
